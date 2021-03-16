3d1f468c84db87e056839deabf9dc0fd8132bfe2
denis0872 <denispetergof@gmail.com>
2021-03-17 00:49:10 +0300

    final

--- WebMVCR1/WebMVCR1/Controllers/HomeController.cs
+++ WebMVCR1/WebMVCR1/Controllers/HomeController.cs
@@ -18,10 +18,28 @@ namespace WebMVCR1.Controllers
             ViewData["Mes"] = "хорошего настроения";
             return View();
         }
+        [HttpGet]
         public ViewResult InputData() 
         { 
+
             return View(); 
         }
+        [HttpPost]
+        public ViewResult InputData(Person p)
+        {
+            db.AddResponse(p);
+            return View("Hello", p);
+           
+        }
+        private static PersonRepository db = new PersonRepository();
+        public ViewResult OutputData() 
+        { 
+            ViewBag.Pers = db.GetAllResponses; 
+            ViewBag.Count = db.NumberOfPerson; 
+            return View("ListPerson");
+        }
+
+
         //public string Index(string hel)
         //    {
         //    string Greeting = ModelClass.ModelHello() + 
