f8c0176a9bdfe0bb2e4783297cc688e2aeb49e2b
denis0872 <denispetergof@gmail.com>
2021-03-18 01:01:34 +0300

    Lab5-part1

--- MvcCreditApp1/MvcCreditApp1/Controllers/HomeController.cs
+++ MvcCreditApp1/MvcCreditApp1/Controllers/HomeController.cs
@@ -3,13 +3,18 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using MvcCreditApp1.Models;
 
 namespace MvcCreditApp1.Controllers
 {
+   
     public class HomeController : Controller
     {
+        private CreditContext db = new CreditContext();
         public ActionResult Index()
         {
+            var allCredits = db.Credits.ToList<Credit>();
+            ViewBag.Credits = allCredits;
             return View();
         }
 
