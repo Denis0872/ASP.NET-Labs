f529ebc2e49bb51aa581a8c1ced4b851a098de10
denis0872 <denispetergof@gmail.com>
2021-03-19 00:06:02 +0300

    Lab6-task1

--- MvcCreditApp1/MvcCreditApp1/Controllers/HomeController.cs
+++ MvcCreditApp1/MvcCreditApp1/Controllers/HomeController.cs
@@ -7,14 +7,13 @@ using MvcCreditApp1.Models;
 
 namespace MvcCreditApp1.Controllers
 {
-   
+
     public class HomeController : Controller
     {
         private CreditContext db = new CreditContext();
         public ActionResult Index()
         {
-            var allCredits = db.Credits.ToList<Credit>();
-            ViewBag.Credits = allCredits;
+            GiveCredits();
             return View();
         }
 
@@ -30,6 +29,31 @@ namespace MvcCreditApp1.Controllers
             ViewBag.Message = "Your contact page.";
 
             return View();
+        }
+        private void GiveCredits()
+        {
+            var allCredits = db.Credits.ToList<Credit>();
+            ViewBag.Credits = allCredits;
+        }
+        [HttpGet]
+        public ActionResult CreateBid()
+        {
+            GiveCredits();
+            var allBids = db.Bids.ToList<Bid>();
+            ViewBag.Bids = allBids;
+            return View();
+        }
+        [HttpPost]
+        public string CreateBid(Bid newBid)
+        {
+            newBid.bidDate = DateTime.Now;
+            // Добавляем новую заявку в БД
+            db.Bids.Add(newBid);
+            // Сохраняем в БД все изменения
+            db.SaveChanges();
+            return "Спасибо, <b>" + newBid.Name + 
+                "</b>, за выбор нашего банка." +
+                " Ваша заявка будет рассмотрена в течении 10 дней.";
         }
     }
 }
