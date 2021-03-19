814ca843e999a7167c172a5baa2e06bc42b543f2
denis0872 <denispetergof@gmail.com>
2021-03-19 11:02:33 +0300

    Lab6-final

--- MvcCreditApp1/MvcCreditApp1/Models/Credit.cs
+++ MvcCreditApp1/MvcCreditApp1/Models/Credit.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Web;
 
@@ -7,10 +9,19 @@ namespace MvcCreditApp1.Models
 {
     public class Credit
     {
-         public virtual int CreditId { get; set; }
-         public virtual string Head { get; set; } 
-         public virtual int Period { get; set; } 
-         public virtual int Sum { get; set; } 
-         public virtual int Procent { get; set; }
+        
+        public virtual int CreditId { get; set; }
+        [DisplayName("Имя Название")]
+        [Required]
+        public virtual string Head { get; set; }
+        [DisplayName("Период")]
+        [Required]
+        public virtual int Period { get; set; }
+        [DisplayName("Сумма кредита")]
+        [Required]
+        public virtual int Sum { get; set; }
+        [DisplayName("Процентная ставка")]
+        [Required]
+        public virtual int Procent { get; set; }
     }
 }
